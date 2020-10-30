97247deadc60da24637b9189e835add8ea9ba81d
jwindhaber <juergen.windhaber@gmail.com>
2020-10-30 13:50:03 +0100

    (Structure) Initial commit for a simple spring boot app k8s deployment

---
+++ src/main/java/easytax/docker/example/simpleweb/SimpleController.java
@@ -0,0 +1,18 @@
+package easytax.docker.example.simpleweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author juergen.windhaber
+ * @since 18.0.0
+ */
+@RestController
+public class SimpleController {
+
+    @GetMapping(path = "/hello")
+    public String sayHello() {
+        return "Hello you old fart!";
+    }
+
+}
